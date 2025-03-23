2d551dc182007418237993ca150cc4955478bbca
CarlosVLing <carlos.valero.ling@gmail.com>
2025-03-23 23:52:38 +0100

    Add files via upload

---
+++ EXTRACTOR DE SUBTÍTOLS.sh
@@ -0,0 +1,2 @@
+yt-dlp -F https://www.apuntmedia.es/programes/zoom/complets/video-zoom-turisme_134_1730811.html
+yt-dlp --write-subs -f hls-audio-0-en__Main_-0 https://www.apuntmedia.es/programes/zoom/complets/video-zoom-turisme_134_1730811.html
